d0f88f8dfa627ee6f6e5cadddaa75f5bb471b1f1
Tom Alexander <me@tomalexander.co.nz>
2019-02-05 11:52:14 -0800

    Add LOGGLY_ENV

--- run.sh
+++ run.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 
-# Fail if LOGGLY_AUTH_TOKEN is not set
-if [ -z "$LOGGLY_AUTH_TOKEN" ]; then
-  if [ ! -z "$TOKEN" ]; then
-    # grandfather old env var
-    export LOGGLY_AUTH_TOKEN=$TOKEN
-  else
+if [ -z "$LOGGLY_ENV" ]; then
+  echo "Missing \$LOGGLY_ENV"
+  exit 1
+fi
+
+if [ "$LOGGLY_ENV" != "development" ]; then
+  if [ -z "$LOGGLY_AUTH_TOKEN" ]; then
     echo "Missing \$LOGGLY_AUTH_TOKEN"
     exit 1
   fi
-fi
 
-# Fail if LOGGLY_TAG is not set
-if [ -z "$LOGGLY_TAG" ]; then
-  if [ ! -z "$TAG" ]; then
-    # grandfather old env var
-    export LOGGLY_TAG=$TAG
-  else
+  if [ -z "$LOGGLY_TAG" ]; then
     echo "Missing \$LOGGLY_TAG"
     exit 1
   fi
@@ -25,18 +20,18 @@ fi
 # Create spool directory
 mkdir -p /var/spool/rsyslog
 
-# If LOGGLY_DEBUG is true, write logs to stdout as well
-if [ "$LOGGLY_DEBUG" = true ]; then
-  sed -i "/\*\.\* @@logs-01\.loggly\.com.*/a \*\.\* \:omstdout\:" /etc/rsyslog.conf
-fi
-
 # Expand multiple tags, in the format of tag1:tag2:tag3, into several tag arguments
-LOGGLY_TAG=$(echo "$LOGGLY_TAG" | sed 's/:/\\\\" tag=\\\\"/g')
+LOGGLY_TAG=$(echo "$LOGGLY_TAG:$LOGGLY_ENV" | sed 's/:/\\\\" tag=\\\\"/g')
 
 # Replace variables
 sed -i "s/LOGGLY_AUTH_TOKEN/$LOGGLY_AUTH_TOKEN/" /etc/rsyslog.conf
 sed -i "s/LOGGLY_TAG/$LOGGLY_TAG/" /etc/rsyslog.conf
 
+if [ "$LOGGLY_ENV" = "development" ]; then
+  echo "*.* :omstdout:" >> /etc/rsyslog.conf
+else
+  echo "*.* @@logs-01.loggly.com:6514;LogglyFormat" >> /etc/rsyslog.conf
+fi
+
 # Run RSyslog daemon
 exec /usr/sbin/rsyslogd -n
-
